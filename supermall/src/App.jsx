3c0ac5c4a2cbfff43955f901a948486193e45b19
Attainy <111291076+Attainy@users.noreply.github.com>
2024-03-12 02:24:26 +0900

    페이지 구성

---
+++ supermall/src/App.jsx
@@ -0,0 +1,49 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./pages/Root";
+import Error from "./pages/Error";
+import Main from "./pages/Main";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import MyPage from "./pages/MyPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <Error />,
+        childeren: [
+            {
+                index: true,
+                element: <Main />,
+            },
+            {
+                path: "/products",
+                element: <Products />,
+            },
+            {
+                path: "/products/:id",
+                element: <ProductDetail />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+            {
+                path: "/mypage",
+                element: <MyPage />,
+            },
+        ],
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
